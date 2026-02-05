dc2b8b65a6a0f735241d492271ce45a7b8ad7c2a
kelly <kelly@kellys-Mac-Studio.local>
2026-02-05 10:18:30 -0600

    Migrate to pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
   },
   webServer: {
-    command: `npm run start -- --port ${port}`,
+    command: `pnpm run start --port ${port}`,
     url: `http://127.0.0.1:${port}`,
     reuseExistingServer,
     timeout: 120_000,
